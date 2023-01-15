6daf753b7780c4e02dd4da25978d623ff9ce5109
Marcos <marcos.mrpb@gmail.com>
2023-01-15 18:02:27 -0300

    tirei html

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Pokemon</title>
-
-    </head>
-    <body>
-        <h1>"Olá {{$name}}"</h1>
-    </body>
-</html>
+<h1>"Olá {{$name}}"</h1>
